d59d6f62c377dd0158f0f5465956c6f1adf1d4d5
mgfebrian <44362621+mgfebrian@users.noreply.github.com>
2026-08-02 22:17:30 +0700

    feat(serial-echo): read and write serial echo

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,28 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use embedded_hal::{delay::DelayNs, digital::InputPin};
-use microbit::{Board, hal::{Temp, Timer}};
+use microbit::{Board, hal::uarte};
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::{rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
     let board = Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
-    let mut button_a = board.buttons.button_a;
-    let mut thermostat = Temp::new(board.TEMP); 
 
-    rprintln!("Thermostat App Ready! Press Button A");
+    let mut serial  = uarte::Uarte::new(
+        board.UARTE0, 
+        board.uart.into(), 
+        uarte::Parity::EXCLUDED, 
+        uarte::Baudrate::BAUD115200
+    );
+
+    
     loop {
-        if button_a.is_low().unwrap() {
-            rprintln!("Temperature: {}", thermostat.measure().to_num::<f32>());
+        let mut buffer = [0u8; 1];
+        if serial.read(&mut buffer).is_ok() {
+            serial.write(&buffer).unwrap();
         }
-        timer.delay_ms(500);
     }
 }
